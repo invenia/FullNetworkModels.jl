67efeca835380cc3913f69601be331c208bfbfa8
Raphael Saavedra <raphael.saavedra@invenialabs.co.uk>
2020-10-27 14:50:55 +0000

    Improve coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,10 @@ using JuMP
 using PowerSystems
 using Test
 
+const TEST_SYSTEM = fake_3bus_system(MISO)
+
 @testset "InHouseFNM.jl" begin
     include("api.jl")
     include("variables.jl")
+    include("templates.jl")
 end
--- test/variables.jl
+++ test/variables.jl
@@ -1,24 +1,33 @@
+function tests_thermal_generation(fnm)
+    # Check if variables named `p` were created with the correct indices
+    @test issetequal(
+        fnm.model[:p].axes[1],
+        InHouseFNM._get_unit_codes(ThermalGen, fnm.system)
+    )
+    @test issetequal(fnm.model[:p].axes[2], 1:24)
+    return nothing
+end
+
+function tests_commitment(fnm)
+    # Check if variables named `u` were created with the correct indices
+    @test issetequal(
+        fnm.model[:u].axes[1],
+        InHouseFNM._get_unit_codes(ThermalGen, fnm.system)
+    )
+    @test issetequal(fnm.model[:u].axes[2], 1:24)
+    # Check if the created variables are binary
+    @test all(is_binary, fnm.model[:u].data)
+    return nothing
+end
+
 @testset "Variables" begin
-    system = fake_3bus_system(MISO)
-    fnm = FullNetworkModel(system, GLPK.Optimizer)
+    fnm = FullNetworkModel(TEST_SYSTEM, GLPK.Optimizer)
     @testset "add_thermal_generation!" begin
         add_thermal_generation!(fnm)
-        # Check if variables named `p` were created with the correct indices
-        @test issetequal(
-            fnm.model[:p].axes[1],
-            InHouseFNM._get_unit_codes(ThermalGen, fnm.system)
-        )
-        @test issetequal(fnm.model[:p].axes[2], 1:24)
+        tests_thermal_generation(fnm)
     end
     @testset "add_commitment!" begin
         add_commitment!(fnm)
-        # Check if variables named `u` were created with the correct indices
-        @test issetequal(
-            fnm.model[:u].axes[1],
-            InHouseFNM._get_unit_codes(ThermalGen, fnm.system)
-        )
-        @test issetequal(fnm.model[:u].axes[2], 1:24)
-        # Check if the created variables are binary
-        @test all(is_binary, fnm.model[:u].data)
+        tests_commitment(fnm)
     end
 end
