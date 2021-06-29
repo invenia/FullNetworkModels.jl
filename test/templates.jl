b468c17ed1f7f4b44fd60bcc3c17d6fbd7391c33
Raphael Saavedra <raphael.saavedra@invenialabs.co.uk>
2021-06-29 15:48:35 +0100

    Move template tests and add tests for UC no ramps

--- test/templates.jl
+++ test/templates.jl
@@ -16,4 +16,30 @@
         tests_ramp_rates(fnm)
         tests_energy_balance(fnm)
     end
+    @testset "unit_commitment_soft_ramps" begin
+        # Modify system so that hard ramp constraints result in infeasibility
+        system_infeasible = deepcopy(TEST_SYSTEM)
+        gens = collect(get_components(ThermalGen, system_infeasible))
+        gens[1].active_power = 50.0
+        @test get_initial_generation(system_infeasible)[7] == 50.0
+
+        fnm = unit_commitment(system_infeasible, GLPK.Optimizer)
+        optimize!(fnm)
+        # Should be infeasible
+        @test termination_status(fnm.model) == TerminationStatusCode(2)
+
+        # Now do the same with soft ramp constraints – should be feasible
+        fnm_soft_ramps = unit_commitment_soft_ramps(system_infeasible, GLPK.Optimizer)
+        optimize!(fnm_soft_ramps)
+        @test termination_status(fnm_soft_ramps.model) == TerminationStatusCode(1)
+        obj_soft_ramps = objective_value(fnm_soft_ramps.model)
+
+        # Now do the same for no ramp constraints - should be feasible and have a lower
+        # objective value (since there's no penalty for violating soft constraints)
+        fnm_no_ramps = unit_commitment_no_ramps(system_infeasible, GLPK.Optimizer)
+        optimize!(fnm_no_ramps)
+        @test termination_status(fnm_no_ramps.model) == TerminationStatusCode(1)
+        obj_no_ramps = objective_value(fnm_no_ramps.model)
+        @test obj_no_ramps < obj_soft_ramps
+    end
 end
