a6e82226631b34ccb065e1566e4f7aeb3c7f1670
abraham.alvarez.bustos <abraham.alvarez.bustos@invenialabs.co.uk>
2021-07-06 07:04:57 +0100

    WIP con_generation_limits ed

--- src/model/constraints.jl
+++ src/model/constraints.jl
@@ -8,8 +8,9 @@ function con_regulation_requirements! end
 function _con_ancillary_ramp_rates! end
 function _con_generation_ramp_rates! end
 
-function _latex(::typeof(con_generation_limits!); commitment::Bool)
+function _latex(::typeof(con_generation_limits!); commitment::Bool, study::String = "commitment")
     u_gt = commitment ? "u_{g, t}" : ""
+    study == "dispatch" && u_gt = "U_{g, t}"
     return """
         ``P^{\\min}_{g, t} $u_gt \\leq p_{g, t} \\leq P^{\\max}_{g, t} $u_gt, \\forall g \\in \\mathcal{G}, t \\in \\mathcal{T}``
         """
@@ -28,20 +29,41 @@ $(_latex(con_generation_limits!; commitment=false))
 
 if `fnm.model` does not have commitment.
 
+
+$(_latex(con_generation_limits!; study = "dispatch"))
+
+if `fnm.model` has commitment as a Parameter (Economic Dispatch).
+
+
 The constraints added are named `generation_min` and `generation_max`.
 """
 function con_generation_limits!(fnm::FullNetworkModel)
     model = fnm.model
     system = fnm.system
     @assert has_variable(model, "p")
-    _con_generation_limits_commitment!(
-        model,
-        Val(has_variable(model, "u")),
-        get_unit_codes(ThermalGen, system),
-        get_forecast_horizon(system),
-        get_pmin(system),
-        get_pmax(system),
+    gens = collect(get_components(ThermalStandard, system))
+    is_u_param = all(
+        in(get_time_series_values(SingleTimeSeries, gens[1], "status")).([0, 1])
     )
+    if is_u_param
+        _con_generation_limits_dispatch!(
+            model,
+            Val(has_variable(model, "u")),
+            get_unit_codes(ThermalGen, system),
+            get_forecast_horizon(system),
+            get_pmin(system),
+            get_pmax(system),
+        )
+    else
+        _con_generation_limits_commitment!(
+            model,
+            Val(has_variable(model, "u")),
+            get_unit_codes(ThermalGen, system),
+            get_forecast_horizon(system),
+            get_pmin(system),
+            get_pmax(system),
+        )
+    end
     return fnm
 end
 
@@ -256,6 +278,21 @@ function _con_generation_limits_commitment!(model::Model, ::Val{false}, unit_cod
         generation_max[g in unit_codes, t in 1:n_periods],
         p[g, t] <= Pmax[g][t]
     )
+end
+
+function _con_generation_limits_dispatch!(model::Model, U, unit_codes, n_periods, Pmin, Pmax)
+    p = model[:p]
+    @constraint(
+        model,
+        generation_min[g in unit_codes, t in 1:n_periods],
+        Pmin[g][t] * U[g, t] <= p[g, t]
+    )
+    @constraint(
+        model,
+        generation_max[g in unit_codes, t in 1:n_periods],
+        p[g, t] <= Pmax[g][t] * U[g, t]
+    )
+    return model
 end
 
 function _con_ancillary_max!(model::Model, unit_codes, n_periods, Pmax, Pregmax)
